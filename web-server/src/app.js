eb6f32b41c1c6774be679b6003d12133d76ddded
amit bansal <amitbansal@cqlsys.co.uk>
2019-09-03 15:12:24 +0530

    commit2

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,35 @@
 const express = require('express')
 const path = require('path')
-
+//for partials i.e for includes like header and footer
+const hbs = require('hbs')
 
 // console.log(__dirname)
 // console.log(path.join(__dirname, '..//public'))
 
 const app = express()
 
+const geocodePath = path.join(__dirname, '../../weather-app/utils/geocode');
+
+const forecastPath = path.join(__dirname, '../../weather-app/utils/geocode');
+
+
+const geocode = require(geocodePath);
+const forecastcode = require(forecastPath);
+
+
+
+//define paths for express config 
 const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+hbs.registerPartials(partialsPath)
 
-app.set('view engine','hbs')
 
+
+//setup handlebars and config different   
+app.set('view engine', 'hbs')  
+app.set('views',viewsPath)
+ 
 app.use(express.static(publicDirPath))
 
 
@@ -27,18 +46,12 @@ app.get('/help', (req, res) =>
     res.render('help', {
         title: 'Help Page',
         message: 'This is the help page',
+        name: 'Amit',
+        
     })
 })
 
-
-
-//req is request and res is response
-
-// app.get('', (req, res) =>{
-//      res.send('<h1>Hello Express</h1>')
-// })
-
-app.get('/help', (req, res) =>
+app.get('/help', (req, res) => 
 {
     res.send({
         name: 'Amit',
@@ -54,17 +67,67 @@ app.get('/about', (req, res) =>
     })
 })
 
-app.get('/weather', (req, res) =>
-{
+// app.get('/weather', (req, res) =>
+// {
+//     if (!(req.query.address)) {
+//         return res.send({
+//             error: 'You must provide a search term',
+//            })         
+//     }
+
+//     const address = req.query.address
+    
+//     const data = geocode(address, (error,{latitude,longitude,location}) => {
+//     if (error) {
+//         return console.log('error', error); 
+//         }
+    
+//         forecastcode(latitude, longitude, (error, data) =>
+//         {
+//             if (error) {
+//                 return console
+//             }
+//             return {
+//                 data,
+//                 location
+//             }
+//         })
+    
+//     })
+
+//     res.send({
+//         data,
+//     })
+// })
+
+app.get('/products', (req, res) =>{
+    
+    if (!(req.query.q)) {
+        return res.send({
+            error: 'You must provide a search term',
+           })         
+    }
+    
+    // console.log(req.query.q)
+    
     res.send({
-        location: 'Patiala',
-        forecast: {
-            latitude: 45.34,
-            longitude: 74.36
-        }
+        products: req.query,
     })
 })
 
+app.get('/help/*', (req, res) =>
+{
+    res.render('404', {
+        message: 'Help page not found!',
+    })
+})
+
+app.get('*', (req, res) =>
+{
+    res.render('404',{
+        message: 'Page not found'
+    })
+})
 
 app.listen(3000, () =>
 {
